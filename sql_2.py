ca1e8c474ce894d4d6ec7e1f61b8f05b4ad80f42
ryanmars <ryanmars@umich.edu>
2018-02-09 13:38:31 -0500

    Changed so that random string is all integers, not sure if this will help or not

--- sql_2.py
+++ sql_2.py
@@ -1,6 +1,5 @@
 from pymd5 import md5
-import urllib
-import random
+from random import randint
 import string
 
 def find_sub(text):
@@ -12,7 +11,8 @@ i = 0
 string_found = 0
 
 while not string_found:
-	text = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(100)])
+	text = str(randint(0,1000000000000000000000000000000000000000))
+	#text = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(100)]
 	md5_text = md5(text)
 	md5_text = md5_text.digest()
 	string_found = find_sub(md5_text)
