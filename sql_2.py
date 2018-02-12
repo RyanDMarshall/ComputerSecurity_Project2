2dd0b9c37f3de4001d106edd70fe582ccd9e8785
ryanmars <ryanmars@umich.edu>
2018-02-12 02:07:42 -0500

    Started Part 2

--- sql_2.py
+++ sql_2.py
@@ -1,10 +1,9 @@
 from pymd5 import md5
 from random import randint
-import string
+import string, urllib
 
 def find_sub(text):
-	str_idx = text.find("'='#")
-	return (str_idx != -1)
+	return "'='#" in text
 
 i = 0
 string_found = 0
@@ -14,7 +13,7 @@ while not string_found:
 	#text = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(100)]
 	md5_text = md5(text)
 	md5_text = md5_text.digest()
-	string_found = find_sub(md5_text)
+	string_found = ("'='#" in text)
 	i += 1
 	if (i % 1000000 == 0):
 		print i
